9e33a85544f5ebd4ba2f82ee4d453a6dbdfba89b
Teo Gonzalez <teo.gonzalez.c@hotmail.com>
2019-09-26 16:09:14 -0600

    Reindent and clean files

--- docs/index.js
+++ docs/index.js
@@ -2,95 +2,96 @@
 const feedEle = document.getElementById("feed");
 
 function changeColor(colors) {
-    document.body.style.backgroundColor =
-        colors[Math.floor(Math.random() * colors.length)];
+	document.body.style.backgroundColor =
+	colors[Math.floor(Math.random() * colors.length)];
 }
 
 function fillFeed(feed) {
-    let time = document.getElementById("time");
-    time.style.marginTop = "2%";
-    time.style.padding = "20px";
-
-    console.log(feed);
-
-    feed.items.forEach((item) =>{
-        let name = document.createElement("a");
-        name.className = "item";
-        name.href = item.link;
-        name.innerText = "";
-        name.target = "_blank";
-        if (item.hasOwnProperty("creator")) {
-            name.innerText += feed.title + " | " + item.title;
-        }
-
-    feedEle.appendChild(name);
-    })
+	let time = document.getElementById("time");
+	time.style.marginTop = "2%";
+	time.style.padding = "20px";
+	
+	console.log(feed);
+	
+	feed.items.forEach((item) =>{
+		let name = document.createElement("a");
+		name.className = "item";
+		name.href = item.link;
+		name.innerText = "";
+		name.target = "_blank";
+		if (item.hasOwnProperty("creator")) {
+			name.innerText += feed.title + " | " + item.title;
+		}
+		
+		feedEle.appendChild(name);
+	})
 }
 
 function changeTime(time, date) {
-    document.getElementById("t").innerText = moment().format(time);
-    document.getElementById("d").innerText = moment().format(date);
+	document.getElementById("t").innerText = moment().format(time);
+	document.getElementById("d").innerText = moment().format(date);
 }
 
 async function getOpt(opt) {
-    if (window.hasOwnProperty("browser")) {
-        let res = await browser.storage.local.get(opt);
-        return res[opt];
-    } else {
-        if (["colors", "shuffle"].includes(opt)) {
-            return JSON.parse(localStorage[opt]);
-        } else {
-            return localStorage[opt];
-        }
-    }
+	if (window.hasOwnProperty("browser")) {
+		let res = await browser.storage.local.get(opt);
+		return res[opt];
+	} else {
+		if (["colors", "shuffle"].includes(opt)) {
+			return JSON.parse(localStorage[opt]);
+		} else {
+			return localStorage[opt];
+		}
+	}
 }
 
 async function main() {
-    // for web demo
-    if (!window.hasOwnProperty("browser")) {
-        console.log("Ex: localStorage.feed = 'https://reddit.com/r/popular/.rss'");
-        let storage = await (await fetch("options.json")).json();
-        if (localStorage !== null) {
-            localStorage.colors = JSON.stringify(storage.colors);
-            localStorage.feed = JSON.stringify(storage.feed);
-            localStorage.favourites = JSON.stringify(storage.favourites);
-
-            localStorage.date = storage.date;
-            localStorage.time = storage.time;
-            localStorage.shuffle = storage.shuffle;
-        }
-    }
-
-    // set color
-    let colors = await getOpt("colors");
-    let shuffle = await getOpt("shuffle");
-    changeColor(colors);
-    // change color every 5s if shuffle is enabled
-    if (shuffle) {
-        setInterval(changeColor.bind(null, colors), 5000);
-        document.body.style.transition = "5s";
-    }
-
-    // change time
-    let date = await getOpt("date");
-    let time = await getOpt("time");
-    changeTime(time, date);
-    setInterval(changeTime.bind(null, time, date), 1000);
-
-    // get rss feed
-    let parser = new RSSParser();
-    // use cors proxy on web demo
-    let CORS = !window.hasOwnProperty("browser")
-        ? "https://cors-anywhere.herokuapp.com/"
-        : "";
-        
-    feeds = JSON.parse(await getOpt("feed"));
-    feeds.forEach(
-        async (item) => {
-            let feed = await parser.parseURL(CORS + item);
-            fillFeed(feed)
-        }
-    )
-}
-
-document.addEventListener("DOMContentLoaded", main);
+	// for web demo
+	if (!window.hasOwnProperty("browser")) {
+		console.log("Ex: localStorage.feed = 'https://reddit.com/r/popular/.rss'");
+		let storage = await (await fetch("options.json")).json();
+		if (localStorage !== null) {
+			localStorage.colors = JSON.stringify(storage.colors);
+			localStorage.feed = JSON.stringify(storage.feed);
+			localStorage.favourites = JSON.stringify(storage.favourites);
+			
+			localStorage.date = storage.date;
+			localStorage.time = storage.time;
+			localStorage.shuffle = storage.shuffle;
+		}
+	}
+	
+	// set color
+	let colors = await getOpt("colors");
+	let shuffle = await getOpt("shuffle");
+	changeColor(colors);
+	// change color every 5s if shuffle is enabled
+	if (shuffle) {
+		setInterval(changeColor.bind(null, colors), 5000);
+		document.body.style.transition = "5s";
+	}
+	
+	// change time
+	let date = await getOpt("date");
+	let time = await getOpt("time");
+	changeTime(time, date);
+	setInterval(changeTime.bind(null, time, date), 1000);
+	
+	// get rss feed
+	let parser = new RSSParser();
+	// use cors proxy on web demo
+	let CORS = !window.hasOwnProperty("browser")
+	? "https://cors-anywhere.herokuapp.com/"
+	: "";
+	
+	feeds = JSON.parse(await getOpt("feed"));
+	feeds.forEach(
+		async (item) => {
+			let feed = await parser.parseURL(CORS + item);
+			fillFeed(feed)
+		}
+		)
+	}
+	
+	document.addEventListener("DOMContentLoaded", main);
+	
--- docs/options.js
+++ docs/options.js
@@ -1,3 +1,4 @@
+// TODO: Change this to another file
 const shuffle = document.getElementById("shuffle");
 const colors = document.getElementById("colors");
 const feed = document.getElementById("feed");
@@ -5,22 +6,22 @@ const date = document.getElementById("date");
 const time = document.getElementById("time");
 
 async function load() {
-    let res = await browser.storage.local.get();
-    shuffle.value = res.shuffle;
-    colors.value = res.colors.join(",");
-    feed.value = res.feed;
-    date.value = res.date;
-    time.value = res.time;
+	let res = await browser.storage.local.get();
+	shuffle.value = res.shuffle;
+	colors.value = res.colors.join(",");
+	feed.value = res.feed;
+	date.value = res.date;
+	time.value = res.time;
 }
 
 function save() {
-    browser.storage.local.set({
-        shuffle: JSON.parse(shuffle.value),
-        colors: colors.value.split(","),
-        feed: feed.value,
-        date: date.value,
-        time: time.value
-    });
+	browser.storage.local.set({
+		shuffle: JSON.parse(shuffle.value),
+		colors: colors.value.split(","),
+		feed: feed.value,
+		date: date.value,
+		time: time.value
+	});
 }
 
 document.addEventListener("DOMContentLoaded", load);
